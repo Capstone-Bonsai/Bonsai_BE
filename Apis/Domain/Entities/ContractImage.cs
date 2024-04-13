b2c0cb296d0878d671631cd2331a9af61428c8b6
dothanhbo <112932777+dothanhbo@users.noreply.github.com>
2024-04-13 08:58:21 +0700

    [][]

--- Apis/Domain/Entities/ContractImage.cs
+++ Apis/Domain/Entities/ContractImage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
@@ -12,6 +13,7 @@ namespace Domain.Entities
         [ForeignKey("Contract")]
         public Guid ContractId { get; set; }
         public string Image { get; set; }
+        [JsonIgnore]
         public virtual Contract Contract { get; set; }
     }
 }
