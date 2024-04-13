9ea3bcc1cd15558ece2890af8eed7e088ec1e786
LAPTOP-D5RP13A1\tramy <tramygeo@gmail.com>
2024-04-13 11:18:11 +0700

    [Mynguyen][fix ]

--- Apis/Domain/Enums/ContractStatus.cs
+++ Apis/Domain/Enums/ContractStatus.cs
@@ -8,6 +8,7 @@ namespace Domain.Enums
 {
     public enum ContractStatus
     {
-        Waiting =1, Paid = 2, Processing = 3, Fail = 4, Canceled = 5, TaskFinished = 6 , Completed = 7, Complained = 8, ProcessingComplaint = 9, ProcessedComplaint = 10, DoneTaskComplaint = 11
+        Waiting =1, Paid = 2, Processing = 3, Fail = 4, Canceled = 5, TaskFinished = 6 , 
+        Completed = 7, Complained = 8, ProcessingComplaint = 9, ProcessedComplaint = 10, DoneTaskComplaint = 11
     }
 }
