b8c9b631933e443c133131d0c5897b74e8724097
dothanhbo <112932777+dothanhbo@users.noreply.github.com>
2024-04-18 23:04:14 +0700

    [][]

--- Apis/Application/Interfaces/INotificationService.cs
+++ Apis/Application/Interfaces/INotificationService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Commons;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +12,6 @@ namespace Application.Interfaces
     {
         Task SendMessageForUserId(Guid id, string message);
         Task SendToStaff(string message);
+        Task<Pagination<Notification>> GetNotification(bool isCustomer, Guid customerId, int pageIndex, int pageSize);
     }
 }
---
+++ Apis/WebAPI/Controllers/NotificationController.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+        private readonly IClaimsService _claims;
+
+        public NotificationController(INotificationService notificationService,
+            IClaimsService claimsService)
+        {
+            _notificationService = notificationService;
+            _claims = claimsService;
+        }
+        [HttpGet("Pagination")]
+        [Authorize]  
+        public async Task<IActionResult> GetPagination([FromQuery] int pageIndex, int pageSize)
+        {
+            try
+            {
+                var notifications = await _notificationService.GetNotification(_claims.GetIsCustomer, _claims.GetCurrentUserId,pageIndex, pageSize);
+                return Ok(notifications);   
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
