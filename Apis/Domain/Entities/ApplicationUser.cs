4e82444a66b36dd262f83b1777a023ff81f9d701
LAPTOP-D5RP13A1\tramy <tramygeo@gmail.com>
2024-04-18 21:58:24 +0700

    [Mynguyen][fix migration]

--- Apis/Domain/Entities/ApplicationUser.cs
+++ Apis/Domain/Entities/ApplicationUser.cs
@@ -16,7 +16,6 @@ namespace Domain.Entities
         public virtual Gardener? Gardener { get; set; }
         public virtual Manager? Manager { get; set; }
         public virtual Staff? Staff { get; set; }
-
-
+        public IList<Notification> Notifications { get; set; }
     }
 }
