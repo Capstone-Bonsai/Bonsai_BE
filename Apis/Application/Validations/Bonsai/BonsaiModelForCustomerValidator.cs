fc82fc701ddc61fe5f9b7fa59052a00f7c5fca4d
dothanhbo <112932777+dothanhbo@users.noreply.github.com>
2024-05-01 10:00:18 +0700

    [][]

--- Apis/Application/Validations/Bonsai/BonsaiModelForCustomerValidator.cs
+++ Apis/Application/Validations/Bonsai/BonsaiModelForCustomerValidator.cs
@@ -13,7 +13,7 @@ namespace Application.Validations.Bonsai
     {
         public BonsaiModelForCustomerValidator()
         {
-            RuleFor(x => x.Address).MaximumLength(200).WithMessage("Tên không quá 200 ký tự.");
+            RuleFor(x => x.Address).MaximumLength(200).WithMessage("Địa chỉ không quá 200 ký tự.");
             RuleFor(x => x.Square).GreaterThan(0).WithMessage("Diện tích vườn lớn hơn 0.");
             RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Danh mục không được để trống.");
 
@@ -21,10 +21,11 @@ namespace Application.Validations.Bonsai
 
             RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Tên không được để trống.")
-                .MaximumLength(50).WithMessage("Tên không quá 50 ký tự.");
+                .MaximumLength(1000).WithMessage("Tên không quá 100 ký tự.");
 
             RuleFor(x => x.Description)
-                .NotEmpty().WithMessage("Mô tả không được để trống.");
+                .NotEmpty().WithMessage("Mô tả không được để trống.")
+                .MaximumLength(2000).WithMessage("Mô tả không quá 2000 ký tự.");
 
             RuleFor(x => x.YearOfPlanting)
                 .GreaterThan(0).WithMessage("Năm phải là số nguyên dương.");
